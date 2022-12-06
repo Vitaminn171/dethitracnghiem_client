c87f209166d56a8d06fe9381db1dbd82f6b94d99
Vitaminn171 <lyquocan171@gmail.com>
2022-12-07 00:09:28 +0700

    update 6/12

--- src/Main.java
+++ src/Main.java
@@ -29,9 +29,6 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) throws IOException {
         FlatLightLaf.setup();
-        
-        
-        new Login().setVisible(true);
         startConnectToServer();
         
     }
