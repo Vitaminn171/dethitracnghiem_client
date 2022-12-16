9ebf0986fd0ac0eda1130d53c77550111ba5ba22
SealTomzz <huynhcamco4@gmail.com>
2022-12-16 21:20:02 +0700

    fix getRank query

--- src/DAL/ResultDAL.java
+++ src/DAL/ResultDAL.java
@@ -73,7 +73,9 @@ public class ResultDAL extends MyDatabaseManager {
     }
 
     public int getRank(int ExamID, String Examinee, String Date) throws SQLException {
-        String query = "SELECT Rank FROM (SELECT ExamID, Examinee, Date, Score, RANK() OVER(ORDER BY Score DESC) Rank FROM result WHERE ExamID = ? ORDER BY Rank) WHERE Examinee = ? AND Date = ?";
+        String query = "SELECT Rank" +
+                        "FROM (SELECT ExamID, Examinee, Date, Score, DENSE_RANK() OVER( ORDER BY Score DESC ) `Rank` FROM result WHERE ExamID = 3) `DATA`" +
+                        "WHERE Examinee = ? AND Date = ?";
         PreparedStatement p = ResultDAL.getConnection().prepareStatement(query);
         p.setInt(1, ExamID);
         p.setString(2, Examinee);
